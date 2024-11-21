30b60b2fb15eea01691c392bd42d5ac01d56ba74
livingcreative <dnkreative@gmail.com>
2024-11-21 02:03:23 +0100

    formatting, simplification, interface changes, still draft

--- include/kparser/kexpression.h
+++ include/kparser/kexpression.h
@@ -20,33 +20,40 @@
 namespace k_parser
 {
 
-    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     //  Operand
-    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     //
     //  example class for operand object
     //      this class has no implementation!
     //      shows interface that real Operand class should implement
-    //  operand can hold a value to perform immediate expression computations (such as constants)
-    //  or it can hold AST node which will be used to build AST while parsing expression
+    //  operand can hold a value to perform immediate expression computations
+    //  (such as constants)
+    //  or it can hold AST node which will be used to build AST while parsing
+    //  expression
 
     class Operand
     {
     public:
         // construct empty (unassigned operand)
         //      real Operand class can implement other constructors in order to
-        //      have specific values and data which will be assigned as a result of parsing
+        //      have specific values and data which will be assigned as a result
+        //      of parsing
         Operand();
 
         // Operand must implement assignment operator
         Operand &operator=(const Operand &other);
+
+        // Determines operand validity
+        // invalid operands can serve as placeholders for "non stop" parsing
+        constexpr operator bool() const noexcept;
     };
 
 
     // operator group defines operator kind, there are three groups
     //      Unary operators act on single operand
     //      Binary operators act on two operands
-    //      Ternary operators act on three operands (e.g. ?: conditional operator)
+    //      Ternary operators act on 3 operands (e.g. ?: conditional operator)
     enum class OperatorGroup
     {
         Unary,
@@ -54,24 +61,27 @@ namespace k_parser
         Ternary
     };
 
-    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     //  Operator
-    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     //
     //  example class for operator object
     //      this class has no implementation!
     //      shows interface that real Operator class should implement
-    //  operator can be applied in-place for immediate computation and return computed value
-    //  or it can return AST node which will contain passed operands as children nodes
+    //  operator can be applied in-place for immediate computation and return
+    //  computed value or it can return AST node which will contain passed
+    //  operands as child nodes
+    //
+    //  definitionT - implementation specific operator definition metadata
 
-    template <typename D>
+    template <typename definitionT>
     class Operator
     {
     public:
         // construct empty (invalid) operator
         Operator();
         // construct operator from given definition
-        Operator(const D &def);
+        Operator(const definitionT &def);
 
         // Apply unary operation
         Operand operator()(const Operand &other) const;
@@ -80,31 +90,34 @@ namespace k_parser
         // Apply ternary operation
         Operand operator()(const Operand &cond, const Operand &a, const Operand &b) const;
 
-        // Determines operator validity, returns true if requested operator has been correctly parsed
-        // expression parser must return empty invalid operator in case of parser error
+        // Determines operator validity, returns true if requested operator has
+        // been correctly parsed. Expression parser must return empty invalid
+        // operator in case of parser error
         constexpr operator bool() const noexcept;
 
         // Determines operator associativity, true for right to left operators
         constexpr bool rtl() const noexcept;
-        // Determines operator precedence level, higher the number - higher the precedence
+        // Determines operator precedence level, higher the number - higher
+        // the precedence
         constexpr int level() const noexcept;
     };
 
 
-    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     //  ExpressionTokenParser
-    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     //
     //  example class of expression token parser interface
     //      this class has no implementation!
-    //      it just shows what functions real ExpressionTokenParser class should implement
+    //      it just shows what functions real ExpressionTokenParser class should
+    //      implement
 
-    template <typename Op, typename D>
+    template <typename operandT, typename definitionT>
     class ExpressionTokenParser
     {
     public:
-        // parse and return operand object
-        Op Operand();
+        // parse and return (primary) operand object
+        operandT Operand();
 
         // parse left bracket for grouping expressions
         // not mandatory, can safely return false if there's no bracket
@@ -114,7 +127,7 @@ namespace k_parser
         bool RightBracket();
 
         // parse and return whether current token matches given operator definition
-        bool Operator(const D &def);
+        const definitionT *Operator(int level, OperatorGroup group);
 
         // parse ternary operator "separator" part (e.g. : token in ?: conditional operator)
         // presense of separator is mandatory, therefore returning false indicates error
@@ -134,115 +147,100 @@ namespace k_parser
     //      Op - operand type
     //      D  - operator definition type
 
-    template <typename P, typename F, typename Op, typename D>
+    template <
+        typename parserT,
+        typename operatorT, typename operandT
+    >
     class ExpressionParser
     {
     public:
-        // minimum required operator definition struct example
-        /*
-        struct OperatorDefinition
-        {
-            OperatorGroup group;
-            int           level;
-        };
-        */
-
-        // construct expression parser instance with given token parser implementation
-        constexpr ExpressionParser(P &tokenparser, const FixedArray<D> &operators) noexcept;
-
         // parse expression
         // returns parsed expression operand, which is depending on implementation
         // might be expression computed value or expression AST node
-        Op Expression();
+        static constexpr operandT Expression(parserT &parser) noexcept;
 
     private:
         // parse operand (value or node)
-        Op Operand();
+        static constexpr operandT Operand(parserT &parser) noexcept;
         // parse operator with requested level and group
-        F Operator(int level, OperatorGroup group);
+        static constexpr operatorT Operator(parserT &parser, int level, OperatorGroup group) noexcept;
         // helper to get operand as possible subexpression result with higher operator precedence
-        Op OperatorOperand(int level);
-
-    private:
-        P                   &p_tokenparser;
-        const FixedArray<D>  p_operators;
+        static constexpr operandT OperatorOperand(parserT &parser, int level) noexcept;
     };
 
+    #define TT template <typename parserT, typename operatorT, typename operandT>
+    #define TTP parserT, operatorT, operandT
 
-    template <typename P, typename F, typename Op, typename D>
-    constexpr ExpressionParser<P, F, Op, D>::ExpressionParser(P &tokenparser, const FixedArray<D> &operators) noexcept :
-        p_tokenparser(tokenparser),
-        p_operators(operators)
-    {}
-
-    template <typename P, typename F, typename Op, typename D>
-    Op ExpressionParser<P, F, Op, D>::Expression()
+    TT constexpr operandT
+    ExpressionParser<TTP>::Expression(parserT &parser) noexcept
     {
         // take expression as operator operand with lowest precedence level
-        return OperatorOperand(0);
+        return OperatorOperand(parser, 0);
     }
 
 
-    template <typename P, typename F, typename Op, typename D>
-    Op ExpressionParser<P, F, Op, D>::Operand()
+    TT constexpr operandT
+    ExpressionParser<TTP>::Operand(parserT &parser) noexcept
     {
         // check if there's a subexpression in brackets
-        auto sub = p_tokenparser.LeftBracket();
+        auto sub = parser.LeftBracket();
 
-        if (sub) {
-            // parse subexpression as independent expression
-            auto op = Expression();
-            // TODO: handle error - upsense of mandatory right bracket
-            p_tokenparser.RightBracket();
-            return op;
-        } else {
+        if (!sub) {
             // parse regular operand
-            return p_tokenparser.Operand();
+            return parser.Operand();
         }
+
+        // parse subexpression as independent expression
+        auto op = Expression(parser);
+
+        if (parser.RightBracket()) {
+            return op;
+        }
+
+        // TODO: handle error - absense of mandatory right bracket
+        return op;
     }
 
-    template <typename P, typename F, typename Op, typename D>
-    F ExpressionParser<P, F, Op, D>::Operator(int level, OperatorGroup group)
+    TT constexpr operatorT
+    ExpressionParser<TTP>::Operator(parserT &parser, int level, OperatorGroup group) noexcept
     {
-        // parse operator
-        auto op = std::find_if(
-            p_operators.begin(), p_operators.end(),
-            [level, group, this](auto &od) {
-                // skip definitions with unwanted group/level
-                if (od.group != group || level > od.level) {
-                    return false;
-                }
-                // try to parse matching operator definition and return it on match
-                return p_tokenparser.Operator(od);
-            }
-        );
+        auto result = parser.Operator(level, group);
 
-        // return empty operator if nothing was parsed or found one otherwise
-        return op == p_operators.end() ? F() : F(*op);
+        if (result != nullptr) {
+            return operatorT(*result);
+        }
+
+        // return empty error operator if required operator was not parsed
+        return operatorT();
     }
 
-    template <typename P, typename F, typename Op, typename D>
-    Op ExpressionParser<P, F, Op, D>::OperatorOperand(int level)
+    TT constexpr operandT
+    ExpressionParser<TTP>::OperatorOperand(parserT &parser, int level) noexcept
     {
-        Op result;
+        operandT result;
 
         // unroll unary operators
-        if (auto opf = Operator(level, OperatorGroup::Unary)) {
-            result = opf(OperatorOperand(opf.level()));
+        if (auto opf = Operator(parser, level, OperatorGroup::Unary)) {
+            result = opf(OperatorOperand(parser, opf.level()));
         } else {
-            result = Operand();
+            result = Operand(parser);
         }
 
         // unroll binary operators
         while (true) {
             // try to get binary operator
-            auto opf = Operator(level, OperatorGroup::Binary);
+            auto opf = Operator(parser, level, OperatorGroup::Binary);
             if (opf) {
-                // apply binary operator to left and right hand side operands and replace left as a result
-                result = opf(result, OperatorOperand(opf.level() + (opf.rtl() ? 0 : 1)));
+                auto level = opf.level() + (opf.rtl() ? 0 : 1);
+                // apply binary operator to left and right hand side operands
+                // and replace left as a result
+                result = opf(
+                    result,
+                    OperatorOperand(parser, level)
+                );
             } else {
                 // try to get ternary operator
-                auto ternary = Operator(level, OperatorGroup::Ternary);
+                auto ternary = Operator(parser, level, OperatorGroup::Ternary);
                 if (!ternary) {
                     // stop expression parsing in case there's no more operators
                     break;
@@ -250,14 +248,14 @@ namespace k_parser
 
                 // left part of ternary operator subexpression (right after operator itself)
                 // parsed as independent subexpression
-                auto a = Expression();
+                auto a = Expression(parser);
 
                 // parse ternary operator separator
                 // TODO: handle error
-                p_tokenparser.TernarySeparator();
+                parser.TernarySeparator();
 
                 // right part of ternary operator subexpression parsed as regular operator operand
-                auto b = OperatorOperand(ternary.level() + 1);
+                auto b = OperatorOperand(parser, ternary.level() + 1);
 
                 // apply ternaryoperator to all three operands
                 result = ternary(result, a, b);
@@ -266,5 +264,8 @@ namespace k_parser
 
         return result;
     }
+
+    #undef TT
+    #undef TTP
 
 } // namespace k_scanner
